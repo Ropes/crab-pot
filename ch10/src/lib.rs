dd6a6758b5be6622a85fa766b2cad0432a89c78e
Josh Roppo <joshroppo@gmail.com>
2021-05-09 15:17:41 -0700

    RLP Ch10: Multi-Generic definitions

--- ch10/src/lib.rs
+++ ch10/src/lib.rs
@@ -27,6 +27,20 @@ impl Summary for NewsArticle{
     }
 }
 
+// accept a trait as function parameter.
+// similar to accepting an interface in Go.
+pub fn notify(item: impl Summary){
+    println!("breaking news! {}", item.summarize())
+}
+
+// trait bound syntax for accepting generic type
+// more verbose
+pub fn notify_summary<T: Summary>(item: T){
+    println!("breaking news: {}", item.summarize())
+}
+
+
+
 pub struct Tweet{
  pub username: String,
  pub content: String,
@@ -40,5 +54,33 @@ impl Summary for Tweet {
     }
     fn summarize(&self) -> String {
         format!("{}: {}", self.username, self.content)
+    }
+}
+/* see page 375 for fix
+fn returns_summarizable(switch: bool) -> impl Summary{
+    if switch{
+        NewsArticle{
+            headline: String::from("Penguins win stanley cup"),
+            location: String::from("Pittsburgh, PA"),
+            author: String::from("Iceburgh"),
+            content: String::from("Pengiuins win again!"),
+        }
+    }else{
+        Tweet{
+            username: String::from("horse_eboosk"),
+            content: String::from("why the long face?"),
+            reply: false,
+            retweet: false,
+        }
+    }
+}
+*/
+
+// longest string returned using parameter lifetime annotation
+pub fn longest<'a>(x: &'a str, y: &'a str) -> &'a str{
+    if x.len() > y.len() {
+        x
+    } else {
+        y
     }
 }
--- ch10/src/main.rs
+++ ch10/src/main.rs
@@ -1,8 +1,7 @@
-
 use ch10::{self, Summary};
 
-fn main(){
-    let tweet = ch10::Tweet{
+fn main() {
+    let tweet = ch10::Tweet {
         username: String::from("horse_ebooks"),
         content: String::from("of course, as you probably already know, people"),
         reply: false,
@@ -10,6 +9,10 @@ fn main(){
     };
 
     println!("1 new tweet: {}", tweet.summarize());
+    let string1 = String::from("long string is long");
+    let string2 = String::from("xyz");
+    let result = ch10::longest(string1.as_str(), string2.as_str());
+    println!("The longest string is '{}'", result);
 }
 
 fn main2() {
@@ -17,19 +20,19 @@ fn main2() {
     let r = largest(&number_list);
     println!("largest number: {}", r);
 
-    let number_list = vec![102, 34, 6000, 89,234,62];
+    let number_list = vec![102, 34, 6000, 89, 234, 62];
     let char_list = vec!['y', 'b', 'd', 'x'];
-    let r = largest(&char_list); 
+    let r = largest(&char_list);
     println!("largest char: {}", r);
 
-    let integer = Point{x: 5, y: 10};
-    let float = Point{x:1.0, y: 3.0};
-    let mutant = Point{x: 3, y: 5.0};
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 3.0 };
+    let mutant = Point { x: 3, y: 5.0 };
 }
 
 fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
     let mut largest = &list[0];
-    for item in list.iter(){
+    for item in list.iter() {
         if item > largest {
             largest = item;
         }
@@ -46,7 +49,7 @@ impl<T, U> Point<T, U> {
     fn x(&self) -> &T {
         return &self.x;
     }
-    fn y(&self) -> &U{
+    fn y(&self) -> &U {
         return &self.y;
     }
 
