997432777bc2bd204491111e261e8064d3a26785
Josh Roppo <joshroppo@gmail.com>
2021-05-18 08:17:38 -0700

    RPL Ch12: Std libs to construct a main

---
+++ ch12/src/main.rs
@@ -0,0 +1,18 @@
+use std::process;
+use std::env;
+use ch12::*;
+
+fn main() {
+    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err|{
+        eprintln!("problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config){
+        eprintln!("application err: {}", e);
+        process::exit(1);
+    }
+}
