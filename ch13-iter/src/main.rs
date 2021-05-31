3247a7942b49d6fdbfb036cd2705c781b2be3612
Josh Roppo <joshroppo@gmail.com>
2021-05-31 10:01:31 -0700

    RPL Ch13: Getting weird with iterators

--- ch13-iter/src/main.rs
+++ ch13-iter/src/main.rs
@@ -8,7 +8,29 @@ fn generate_workout(intensity: u32, random_number: u32) {
         num
     };
 
-    if intensity < 25 {
-        println!("today, do {} pushups!", expensive_closure(intensity));
+    // Different closure definitions
+    fn add_one_v1(x: u32) -> u32 {
+        x + 1
     }
+    let add_one_v2 = |x: u32| -> u32 { x + 1 };
+    let add_one_v3 = |x: u32| x + 1;
+    let add_one_v4 = |x: u32| x + 1;
+
+    if intensity < 25 {
+        println!("today, do {} pushups!", expensive_closure(intensity),);
+        println!("next do {} situps", expensive_closure(intensity),);
+    } else {
+        if random_number == 3 {
+            println!("take a break today")
+        } else {
+            println!("today run for {} minutes", expensive_closure(2),);
+        }
+    }
+}
+
+fn main() {
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    generate_workout(simulated_user_specified_value, simulated_random_number);
 }
